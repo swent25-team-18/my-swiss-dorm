8afc38a77df6db128139b7ae82a81ed8534c58dd
Matboegli <matheo.bogli@epfl.ch>
2025-10-07 22:25:38 +0200

    feat(viewModel); Implement the Sign In ViewModel
    This commit adds the log in mechanism to the application.
    
    The user can log in with a Google account.

--- app/src/main/java/com/android/mySwissDorm/ui/authentification/SignInScreen.kt
+++ app/src/main/java/com/android/mySwissDorm/ui/authentification/SignInScreen.kt
@@ -1,3 +1,5 @@
+package com.android.mySwissDorm.ui.authentification
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,19 +13,39 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.credentials.CredentialManager
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.android.mySwissDorm.R
 import com.android.mySwissDorm.ui.theme.PalePink
 import com.android.mySwissDorm.ui.theme.Typography
 
 
 @Composable
-fun SignInScreen() {
+fun SignInScreen(
+    onSignedIn: () -> Unit = {},
+    onSignUp: () -> Unit = {},
+    authViewModel: SignInViewModel = viewModel(),
+    credentialManager: CredentialManager = CredentialManager.create(LocalContext.current)
+) {
+    val context = LocalContext.current
+    val uiState by authViewModel.uiState.collectAsState()
+
+    LaunchedEffect(uiState.user) {
+        uiState.user?.let {
+            onSignedIn()
+        }
+    }
+
     Scaffold(
         modifier = Modifier.fillMaxSize()
     ) { padding ->
@@ -40,7 +62,11 @@ fun SignInScreen() {
             )
             Spacer(modifier = Modifier.height(16.dp))
             Button(
-                onClick = {},
+                onClick = {
+                    if (!uiState.isLoading) {
+                        authViewModel.signIn(context,credentialManager)
+                    }
+                },
                 shape = RoundedCornerShape(4.dp),
                 colors = ButtonColors(
                     containerColor = PalePink,
@@ -53,7 +79,7 @@ fun SignInScreen() {
             }
             Spacer(modifier = Modifier.height(256.dp))
             TextButton(
-                onClick = {}
+                onClick = onSignUp
             ) {
                 Text(
                     text = stringResource(R.string.create_account_text),

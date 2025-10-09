fb8aaae69b751ec95f7416a339680a8df4a0071a
anreymon <antoine.reymond@epfl.ch>
2025-10-09 17:39:09 +0200

    feat(city): add local repository and provider
    - Added CitiesRepository interface defining methods to retrieve cities
    - Implemented CitiesRepositoryLocal with mock data and real coordinates
    - Added CitiesRepositoryProvider singleton to manage repository access

---
+++ app/src/main/java/com/android/mySwissDorm/model/city/CitiesRepositoryProvider.kt
@@ -0,0 +1,11 @@
+package com.android.mySwissDorm.model.city
+
+/**
+ * Provides a single instance of the repository in the app. `repository` is mutable for testing
+ * purposes.
+ */
+object CitiesRepositoryProvider {
+    private val _repository: CitiesRepository by lazy { CitiesRepositoryLocal() }
+
+    var repository: CitiesRepository = _repository
+}

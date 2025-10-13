00e756f0b1dc6b3f959af9be375e45c968b6d32a
Mansour Kanaan <mansourkanaan@tsf-484-wpa-3-034.epfl.ch>
2025-10-13 19:22:54 +0200

    feat(colors) : Add necessary colors

--- app/src/main/java/com/android/mySwissDorm/ui/theme/Color.kt
+++ app/src/main/java/com/android/mySwissDorm/ui/theme/Color.kt
@@ -6,3 +6,13 @@ val Red0 = Color(0xFFFF6666)
 val White = Color(0xFFFFFFFF)
 val LightGray = Color(0xFFF0F0F0)
 val LightGray0 = Color(0xFFB0BEC5)
+
+val PalePink = Color(0xFFFC6C85)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)

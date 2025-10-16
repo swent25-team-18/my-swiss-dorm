0021663e22bf7e729aa410ee0b05582028492f42
MansourrKanaan <mansour.kanaan@epfl.ch>
2025-10-16 23:39:34 +0200

    feat(update) : Users can now update their profile

--- app/src/main/java/com/android/mySwissDorm/model/profile/Language.kt
+++ app/src/main/java/com/android/mySwissDorm/model/profile/Language.kt
@@ -1,7 +1,12 @@
 package com.android.mySwissDorm.model.profile
 
 /** This enum class represents the supported languages in the application */
-enum class Language {
-  ENGLISH,
-  FRENCH
+enum class Language(val displayLanguage: String) {
+  ENGLISH("English"),
+  FRENCH("Français"),
+  ;
+
+  override fun toString(): String {
+    return displayLanguage
+  }
 }

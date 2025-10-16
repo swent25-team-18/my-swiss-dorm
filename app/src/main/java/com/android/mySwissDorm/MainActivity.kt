3da6f74c2afef34236c148c880faf7808a398a6c
Matboegli <matheo.bogli@epfl.ch>
2025-10-17 01:51:43 +0200

    test(en2end): Add the Epic1 end2end test
    This commit adds the first end2end test of this app.
    
    It also implement the MainActivity.kt with our application wrapped in MySwissDormApp composable.
    
    SecondActivity.kt is deleted since it is useless.

--- app/src/main/java/com/android/mySwissDorm/MainActivity.kt
+++ app/src/main/java/com/android/mySwissDorm/MainActivity.kt
@@ -1,30 +1,41 @@
 package com.android.mySwissDorm
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.credentials.CredentialManager
+import com.android.mySwissDorm.model.authentification.AuthRepository
 import com.android.mySwissDorm.resources.C
+import com.android.mySwissDorm.ui.navigation.AppNavHost
+import com.android.mySwissDorm.ui.navigation.Screen
 import com.android.mySwissDorm.ui.theme.MySwissDormAppTheme
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
+
+  private lateinit var auth: FirebaseAuth
+  private lateinit var authRepository: AuthRepository
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    Log.d("", Screen.topLevel.joinToString { it.name })
     setContent {
       MySwissDormAppTheme {
         // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize().semantics { testTag = C.Tag.main_screen_container },
             color = MaterialTheme.colorScheme.background) {
-              Greeting("Android")
+              MySwissDormApp()
             }
       }
     }
@@ -32,12 +43,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(text = "Hello $name!", modifier = modifier.semantics { testTag = C.Tag.greeting })
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  MySwissDormAppTheme { Greeting("Android") }
+fun MySwissDormApp(
+    context: Context = LocalContext.current,
+    credentialManager: CredentialManager = CredentialManager.create(context),
+) {
+  AppNavHost(
+      isLoggedIn = FirebaseAuth.getInstance().currentUser != null,
+      context = context,
+      credentialManager = credentialManager)
 }

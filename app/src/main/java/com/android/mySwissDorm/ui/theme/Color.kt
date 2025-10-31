a3e9de769df3bf3986e2e567d65d8bd89e98cb34
MansourrKanaan <mansour.kanaan@epfl.ch>
2025-10-31 05:14:55 +0100

    fix : missing colors

--- app/src/main/java/com/android/mySwissDorm/ui/theme/Color.kt
+++ app/src/main/java/com/android/mySwissDorm/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val LightGray1 = Color(0xE6DCDCDC)
 // val LightGray = Color.Gray
 val LightGray = Color(0xFFE3E3E3)
 val PalePink = Color(0xFFFF6666)
+val Red0 = Color(0xFFFF6666)
+val LightGray0 = Color(0xFFB0BEC5)
+val Gray = Color(0xFF888888)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)

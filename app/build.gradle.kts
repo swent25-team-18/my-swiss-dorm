108522d8005285abc4a407b1b9948ec1329b2cc2
Matboegli <matheo.bogli@epfl.ch>
2025-11-09 20:21:26 +0100

    chore: Add storage extension

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -158,6 +158,7 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.storage)
 
     // Credential Manager (for Google Sign-In)
     implementation(libs.credentials)

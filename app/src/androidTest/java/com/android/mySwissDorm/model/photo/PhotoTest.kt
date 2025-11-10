e5a69e85673a3e3ac724ec1d9851f2d5f2be8e0e
Matboegli <matheo.bogli@epfl.ch>
2025-11-10 20:32:54 +0100

    fix: resolve failing test

--- app/src/androidTest/java/com/android/mySwissDorm/model/photo/PhotoTest.kt
+++ app/src/androidTest/java/com/android/mySwissDorm/model/photo/PhotoTest.kt
@@ -13,8 +13,8 @@ class PhotoTest {
   @Test
   fun testCreateNewEmptyPhoto() {
     val context = InstrumentationRegistry.getInstrumentation().targetContext
-    val uid = UUID.randomUUID().toString()
-    val photo = Photo.createNewPhotoOnCache(context = context, fileName = "$uid.jpg")
+    val uid = UUID.randomUUID().toString() + ".jpg"
+    val photo = Photo.createNewPhotoOnCache(context = context, fileName = uid)
     assertEquals(photo.fileName, uid)
 
     val file = File(context.externalCacheDir, Uri.parse(photo.image.toString()).lastPathSegment!!)

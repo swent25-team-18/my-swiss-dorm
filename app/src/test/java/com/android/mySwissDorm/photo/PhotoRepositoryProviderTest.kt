10cd468be25ace48221c8b7afac9d3895aca6c76
Matboegli <matheo.bogli@epfl.ch>
2025-11-12 07:54:27 +0100

    fix: resolve firebase initialization in PhotoRepositoryProviderTest.kt

--- app/src/test/java/com/android/mySwissDorm/photo/PhotoRepositoryProviderTest.kt
+++ app/src/test/java/com/android/mySwissDorm/photo/PhotoRepositoryProviderTest.kt
@@ -2,6 +2,7 @@ package com.android.mySwissDorm.photo
 
 import android.content.Context
 import com.android.mySwissDorm.model.photo.PhotoRepositoryProvider
+import com.google.firebase.FirebaseApp
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,6 +14,7 @@ class PhotoRepositoryProviderTest {
   @Test
   fun checkRepositoryIsNotNullAfterInitialization() {
     val context: Context = RuntimeEnvironment.getApplication()
+    FirebaseApp.initializeApp(context)
     PhotoRepositoryProvider.initialize(context)
     assertNotNull(PhotoRepositoryProvider.local_repository)
   }

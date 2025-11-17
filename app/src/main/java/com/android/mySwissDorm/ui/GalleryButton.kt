d4b37ebd6614bc5546b19b03632191d4923a8620
Matboegli <matheo.bogli@epfl.ch>
2025-11-17 14:15:41 +0100

    feat: Implement a multiple pictures gallery button

--- app/src/main/java/com/android/mySwissDorm/ui/GalleryButton.kt
+++ app/src/main/java/com/android/mySwissDorm/ui/GalleryButton.kt
@@ -3,6 +3,7 @@ package com.android.mySwissDorm.ui
 import android.net.Uri
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
@@ -75,6 +76,48 @@ fun GalleryButton(
 }
 
 @Composable
+fun GalleryButtonMultiplePick(
+    onSelect: (List<Photo>) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: Shape = ButtonDefaults.shape,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    interactionSource: MutableInteractionSource? = null,
+    choosePicturesContract:
+        ActivityResultContract<PickVisualMediaRequest, List<@JvmSuppressWildcards Uri>> =
+        ActivityResultContracts.PickMultipleVisualMedia(),
+    content: @Composable (RowScope.() -> Unit) = {}
+) {
+  val galleryLauncher =
+      rememberLauncherForActivityResult(choosePicturesContract) { uris ->
+        onSelect(
+            uris.map {
+              Photo(
+                  image = it,
+                  fileName = UUID.randomUUID().toString() + it.path!!.substringAfterLast('.'))
+            })
+      }
+  Button(
+      onClick = {
+        galleryLauncher.launch(
+            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+      },
+      modifier = modifier.testTag(tag = C.GalleryButtonTag.TAG),
+      enabled = enabled,
+      shape = shape,
+      colors = colors,
+      elevation = elevation,
+      border = border,
+      contentPadding = contentPadding,
+      interactionSource = interactionSource) {
+        content()
+      }
+}
+
+@Composable
 fun DefaultGalleryButton(
     onSelect: (Photo) -> Unit,
     modifier: Modifier = Modifier,
@@ -104,6 +147,43 @@ fun DefaultGalleryButton(
       contentPadding = contentPadding,
       interactionSource = interactionSource,
       choosePictureContract = choosePictureContract) {
+        Icon(Icons.Default.Photo, null, tint = if (enabled) MainColor else TextColor)
+        Spacer(Modifier.width(8.dp))
+        Text(text = stringResource(R.string.gallery_button_default_text))
+      }
+}
+
+@Composable
+fun DefaultGalleryButtonMultiplePick(
+    onSelect: (List<Photo>) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: Shape = RoundedCornerShape(14.dp),
+    colors: ButtonColors =
+        ButtonColors(
+            containerColor = TextBoxColor,
+            contentColor = MainColor,
+            disabledContentColor = TextColor,
+            disabledContainerColor = TextBoxColor),
+    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    interactionSource: MutableInteractionSource? = null,
+    choosePicturesContract:
+        ActivityResultContract<PickVisualMediaRequest, List<@JvmSuppressWildcards Uri>> =
+        ActivityResultContracts.PickMultipleVisualMedia(),
+) {
+  GalleryButtonMultiplePick(
+      onSelect = onSelect,
+      modifier = modifier,
+      enabled = enabled,
+      shape = shape,
+      colors = colors,
+      elevation = elevation,
+      border = border,
+      contentPadding = contentPadding,
+      interactionSource = interactionSource,
+      choosePicturesContract = choosePicturesContract) {
         Icon(Icons.Default.Photo, null, tint = if (enabled) MainColor else TextColor)
         Spacer(Modifier.width(8.dp))
         Text(text = stringResource(R.string.gallery_button_default_text))

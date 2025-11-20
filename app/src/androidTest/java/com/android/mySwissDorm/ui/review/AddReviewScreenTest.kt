460216f046f11802146b7509cf05e29603c98667
karl younes <karlyounes900@gmail.com>
2025-11-20 02:14:14 +0100

    fix(Add review screen test): fix the text in the assertion

--- app/src/androidTest/java/com/android/mySwissDorm/ui/review/AddReviewScreenTest.kt
+++ app/src/androidTest/java/com/android/mySwissDorm/ui/review/AddReviewScreenTest.kt
@@ -73,7 +73,7 @@ class AddReviewScreenTest : FirestoreTest() {
     composeTestRule.onNodeWithTag("sizeField").performScrollTo().assertIsDisplayed()
 
     composeTestRule
-        .onNodeWithText("Please complete all required fields (valid grade, size, price, etc.).")
+        .onNodeWithText("Please complete all required fields (valid grade, size, price, etc.) or sign in")
         .assertIsDisplayed()
     composeTestRule.onNodeWithText("Submit Review").assertIsNotEnabled()
   }
@@ -134,7 +134,7 @@ class AddReviewScreenTest : FirestoreTest() {
     assertTrue(viewModel.uiState.value.isFormValid)
     composeTestRule.onNodeWithText("Submit Review").assertIsEnabled()
     composeTestRule
-        .onNodeWithText("Please complete all required fields (valid grade, size, price, etc.).")
+        .onNodeWithText("Please complete all required fields (valid grade, size, price, etc.) or sign in.")
         .assertDoesNotExist()
     composeTestRule.onNodeWithText("Submit Review").performClick()
     advanceUntilIdle() // Used AI for this line

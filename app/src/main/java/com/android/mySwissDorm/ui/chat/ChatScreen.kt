92330fcbf906ec622389bab074bf829806bacf1e
MansourrKanaan <mansour.kanaan@epfl.ch>
2025-12-13 13:58:59 +0100

    fix : fix chat screen keyboard ui

--- app/src/main/java/com/android/mySwissDorm/ui/chat/ChatScreen.kt
+++ app/src/main/java/com/android/mySwissDorm/ui/chat/ChatScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
@@ -182,7 +183,11 @@ fun MyChatScreen(
             viewModelFactoryProvider(context, chatClient, channelId, messageLimit)
           }
 
-      chatTheme { messagesScreen(viewModelFactory, onBackClick) }
+      chatTheme {
+        Box(modifier = modifier.fillMaxSize().imePadding()) {
+          messagesScreen(viewModelFactory, onBackClick)
+        }
+      }
     }
   }
 }
